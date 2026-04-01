137df4f3557a874aec97940d6cd52d6bb3d51d48
Parrot <145677799+Parrot871@users.noreply.github.com>
2026-04-01 14:42:46 +0900

    feat: 프로젝트 기본 세팅

---
+++ src/main/java/com/example/gomplay/GomplayApplication.java
@@ -0,0 +1,13 @@
+package com.example.gomplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GomplayApplication.class, args);
+    }
+
+}
