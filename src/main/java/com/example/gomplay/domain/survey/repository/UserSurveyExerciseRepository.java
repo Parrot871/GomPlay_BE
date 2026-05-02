d12b988659d0e7f90f0274657723e87a80b2a8b7
Parrot <145677799+Parrot871@users.noreply.github.com>
2026-05-02 15:58:06 +0900

    feat: survey, schedule 레포지토리 추가

---
+++ src/main/java/com/example/gomplay/domain/survey/repository/UserSurveyExerciseRepository.java
@@ -0,0 +1,9 @@
+package com.example.gomplay.domain.survey.repository;
+
+import com.example.gomplay.domain.survey.entity.UserSurveyExercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface UserSurveyExerciseRepository extends JpaRepository<UserSurveyExercise, Long> {
+    List<UserSurveyExercise> findByUserProfile_Id(Long userId);
+}
