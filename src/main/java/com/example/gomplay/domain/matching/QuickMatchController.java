e00a3846b50e7beeb067111af0f2d1a7e08fc85d
Parrot <145677799+Parrot871@users.noreply.github.com>
2026-05-06 14:08:00 +0900

    feat: quickmatchcontroller 구현

--- src/main/java/com/example/gomplay/domain/matching/QuickMatchController.java
+++ src/main/java/com/example/gomplay/domain/matching/QuickMatchController.java
@@ -1,4 +1,24 @@
 package com.example.gomplay.domain.matching;
 
+import com.example.gomplay.domain.matching.dto.MatchingToggleRequest;
+import com.example.gomplay.domain.matching.dto.MatchingToggleResponse;
+import com.example.gomplay.domain.matching.service.QuickMatchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/match")
 public class QuickMatchController {
+
+    private final QuickMatchService quickMatchService;
+
+    @PatchMapping("/toggle")
+    public ResponseEntity<MatchingToggleResponse> updateMatchingStatus(
+            @AuthenticationPrincipal Long userId,
+            @RequestBody MatchingToggleRequest request) {
+        return ResponseEntity.ok(quickMatchService.updateMatchingStatus(userId, request.isMatching()));
+    }
 }
