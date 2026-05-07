34af3a8306b21a5a4e8750f3c8ff2ba2ddb572e9
Parrot <145677799+Parrot871@users.noreply.github.com>
2026-05-08 00:46:41 +0900

    feat: timeout 위한 스케쥴러 구현

--- src/main/java/com/example/gomplay/domain/matching/repository/MatchRequestRepository.java
+++ src/main/java/com/example/gomplay/domain/matching/repository/MatchRequestRepository.java
@@ -3,6 +3,8 @@ package com.example.gomplay.domain.matching.repository;
 import com.example.gomplay.domain.matching.entity.MatchRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface MatchRequestRepository extends JpaRepository<MatchRequest, Long> {
@@ -10,5 +12,10 @@ public interface MatchRequestRepository extends JpaRepository<MatchRequest, Long
     // 상대방한테 이미 PENDING 요청이 있는지 확인
     Optional<MatchRequest> findByRequester_IdAndOpponent_IdAndStatus(
             Long requesterId, Long opponentId, MatchRequest.MatchRequestStatus status
+    );
+
+    List<MatchRequest> findByStatusAndExpiresAtBefore(
+            MatchRequest.MatchRequestStatus status,
+            LocalDateTime dateTime
     );
 }
