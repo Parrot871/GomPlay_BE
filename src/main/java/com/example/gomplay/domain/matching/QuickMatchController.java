2b058cd392df5ea45551ed524770cd9331d3843e
Parrot <145677799+Parrot871@users.noreply.github.com>
2026-05-08 00:36:51 +0900

    feat: matchrequest 관련 controller 추가 구현

--- src/main/java/com/example/gomplay/domain/matching/QuickMatchController.java
+++ src/main/java/com/example/gomplay/domain/matching/QuickMatchController.java
@@ -1,8 +1,6 @@
 package com.example.gomplay.domain.matching;
 
-import com.example.gomplay.domain.matching.dto.CandidateResponse;
-import com.example.gomplay.domain.matching.dto.MatchingToggleRequest;
-import com.example.gomplay.domain.matching.dto.MatchingToggleResponse;
+import com.example.gomplay.domain.matching.dto.*;
 import com.example.gomplay.domain.matching.service.QuickMatchService;
 import com.example.gomplay.domain.survey.repository.UserSurveyExerciseRepository;
 import com.example.gomplay.domain.survey.repository.UserSurveyRepository;
@@ -41,6 +39,16 @@ public class QuickMatchController {
         return ResponseEntity.ok(ApiResponse.success(
                 "매칭 후보 목록입니다.",
                 quickMatchService.getCandidates(userId)
+        ));
+    }
+
+    @PostMapping("/request")
+    public ResponseEntity<ApiResponse<MatchRequestResponse>> requestMatch(
+            @AuthenticationPrincipal Long userId,
+            @RequestBody MatchRequestDto request) {
+        return ResponseEntity.ok(ApiResponse.success(
+                "매칭 요청을 보냈습니다.",
+                quickMatchService.requestMatch(userId, request)
         ));
     }
 }
